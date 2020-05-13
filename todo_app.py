001bc348e9fd3a8e7adc64b905bb69a696c2c6c0
Caio Lopes <caiodesalopes@gmail.com>
2020-05-12 21:21:15 -0300

    create fixed

--- todo_app.py
+++ todo_app.py
@@ -1,4 +1,3 @@
-
 import json
 from flask import Flask, render_template, jsonify, request
 
@@ -7,14 +6,19 @@ app = Flask('task_app')
 
 def read_tasks():
     with open('tasks.json', 'r') as fp:
-        tasks = json.loads(fp)
+        tasks = json.load(fp)
 
     return tasks
+    
+def save_tasks(tasks):
+    with open('tasks.json', 'w') as fp:
+        json.dump(tasks, fp)
 
 
 @app.route('/')
 def menu():
     return render_template('menu.html')
+
 
 @app.route('/lista')
 def list_tasks():
@@ -28,19 +32,23 @@ def list_tasks_api():
     tasks = read_tasks()
     return jsonify(tasks)
     
-    
+
 @app.route('/api/v1/criar', methods=['POST'])
 def create_task():
+    tasks = read_tasks()
     task = request.get_json()
     if len(tasks) > 0:
         task_id = tasks[-1]['id'] + 1
-
-    tasks.append({
+    
+    new_task = {
         "id": task_id,
         "name": task["name"],
         "done": False,
-    })
-    return jsonify(tasks)
+    }
+    tasks.append(new_task)
+    save_tasks(tasks)
+
+    return jsonify(new_task)
 
 
 #  eh equivalente a isso:
@@ -49,7 +57,6 @@ def create_task():
 
 
 app.run(host="0.0.0.0", debug=True)
-
 
 
 
